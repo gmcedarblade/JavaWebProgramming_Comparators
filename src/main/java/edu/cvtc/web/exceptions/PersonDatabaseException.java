5ba8e2269b20931291b10d3ecfb68c434bb904d2
Greg Cedarblade <gmcedarblade@gmail.com>
2016-11-22 13:55:48 -0600

    Created new exceptions package and new PersonDatabaseException class

---
+++ src/main/java/edu/cvtc/web/exceptions/PersonDatabaseException.java
@@ -0,0 +1,15 @@
+package edu.cvtc.web.exceptions;
+
+/**
+ * @author gcedarblade
+ *
+ */
+public class PersonDatabaseException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -779892139123729928L;
+
+	
+}
