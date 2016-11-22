fd31f24a928209b1501f8716a16fec5a876e8c68
Greg Cedarblade <gmcedarblade@gmail.com>
2016-11-22 14:32:22 -0600

    Added public and private constants and connection function to DBUtility.java

--- src/main/java/edu/cvtc/web/util/DBUtility.java
+++ src/main/java/edu/cvtc/web/util/DBUtility.java
@@ -1,9 +1,28 @@
 package edu.cvtc.web.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author gcedarblade
  *
  */
 public class DBUtility {
 
+	public static final String CONNECTION = "jdbc:sqlit:person.db";
+	public static final int TIMEOUT = 30;
+	
+	private static final String DRIVER_NAME = "org.sqlite.JDBC";
+	
+	public static Connection createConnection() throws ClassNotFoundException, SQLException {
+		
+		// register the driver
+		Class.forName(DRIVER_NAME);
+		
+		// create the database connection
+		return DriverManager.getConnection(CONNECTION);
+		
+	}
+	
 }
